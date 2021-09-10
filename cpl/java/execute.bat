2b9afeed9f5f2b1d0cb8c90f63bc9fbade8a2d57
lmj01 <lmj01@users.noreply.github.com>
2021-09-10 18:07:29 +0800

    update

---
+++ cpl/java/execute.bat
@@ -0,0 +1,4 @@
+REM set clsName=hello
+set clsName=hello
+javac %clsName%.java
+java %clsName%
---
+++ cpl/java/map.java
@@ -0,0 +1,16 @@
+public class map {
+    public class CaseTip {
+        private String content = "";
+        private long id = 0;
+        public void setContent(content) {
+            this.content = content;
+        }
+        public String getContent() {
+            return this.content;
+        }
+        public void setId
+    }
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
