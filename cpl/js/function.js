b88450ee46e6f9685b201de9aa30937324d1bf58
lmj01 <lmj01@users.noreply.github.com>
2022-03-02 18:12:59 +0800

    Update function.js

--- cpl/js/function.js
+++ cpl/js/function.js
@@ -1,4 +1,26 @@
 function funcO(e, t) {
     return Object.prototype.hasOwnProperty.call(e, t)
 }
+/**
+ * 
+undefined has normal variable semantics that not even strict mode can fix and requires run-time look-up. It can be shadowed like any other variable, and the default global variable undefined is not read-only in ES3.
+void 0 is effectively a compile time bulletproof constant for undefined with no look-up requirements. It is like writing null or true, instead of looking up a variable value. It works out of the box without any safety arguments and is shorter to write. It is better in every way.
+ */
+// void是JavaScript的一个operator，
+// void anything, return undefined
+console.log(void 0);
+console.log(void(0))
+console.log(void "hello")
+console.log(void (new Date()))
 
+// Symbol.toStringTag
+// JavaScript内置对象都可以通过Object.prototype.toString.call获得特定类型标签
+// 对于自定义的，可以自行添加标签
+class c1 {}
+console.log(Object.prototype.toString.call(new c1()));
+class c2 {
+    get [Symbol.toStringTag]() {
+        return 'myC2';
+    }
+}
+console.log(Object.prototype.toString.call(new c2()));
