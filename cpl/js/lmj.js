b5c1df1f6258d4d7add9f05a081447bd70543f4a
lmj01 <lmj01@users.noreply.github.com>
2024-04-07 11:49:58 +0800

    update

--- cpl/js/lmj.js
+++ cpl/js/lmj.js
@@ -1,31 +1,62 @@
-let p2023Q4 = [
-    0.3,
-    0.05,
-    0.03,
-    0.15,
-    0.20,
-    0.12,
-    0.05,
-    0.05,
-    0.03,
-    0.02,
-];
-let r2023Q4 = [
-    90,
-    100,
-    100,
-    50,
-    0,
-    98,
-    100,
-    100,
-    100,
-    100,
-]
-const score = p2023Q4.reduce((pv,cv,i)=>{
-    const r1 = cv * r2023Q4[i];
-    const r2 = pv + r1;
-    console.log(r1, r2);
-    return r2;
-}, 0)
-console.log('total -- ', score);
+const q1in2024 = {
+    p: [
+        0.20,
+        0.30,
+        0.15,
+        0.05,
+        0.10,
+        0.10,
+        0.05,
+        0.03,
+        0.02,
+    ],
+    r: [
+        90, 
+        80,
+        40, // 暂停
+        30, // 暂停
+        90,
+        100,
+        100,
+        100,
+        100,
+    ],
+}
+const q4in2023 = {
+    p: [
+        0.3,
+        0.05,
+        0.03,
+        0.15,
+        0.20,
+        0.12,
+        0.05,
+        0.05,
+        0.03,
+        0.02,
+    ],
+    r: [
+        90,
+        100,
+        100,
+        50,
+        0,
+        98,
+        100,
+        100,
+        100,
+        100,    
+    ],
+}
+const arrayList = [q1in2024, q4in2023];
+function toScroe(data) {
+    console.log('one-quater-year')
+    return data.p.reduce((pv,cv,i)=>{
+        const r1 = cv * data.r[i];
+        const r2 = pv + r1;
+        console.log(i, r1, r2)
+        return r2;
+    }, 0)
+}
+
+console.log('total -- ', arrayList.map(e=>toScroe(e)));
